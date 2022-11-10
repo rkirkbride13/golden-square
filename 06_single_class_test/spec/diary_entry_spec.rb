9cfd208352e75fe9a531a9745745838476058138
Daisy <ysiad926@gmail.com>
2022-11-10 14:23:52 +0000

    add contents test

--- 06_single_class_test/spec/diary_entry_spec.rb
+++ 06_single_class_test/spec/diary_entry_spec.rb
@@ -1,9 +1,14 @@
 require 'diary_entry'
 
 RSpec.describe DiaryEntry do
-  it "should initialize a new diary entry" do
+  it "should initialize a new diary entry with a title" do
     entry = DiaryEntry.new("Makers", "some text")
     result = entry.title
     expect(result).to eq "Makers"
+  end
+  it "should initialize a new diary entry with a contents" do
+    entry = DiaryEntry.new("Makers", "some text")
+    result = entry.contents
+    expect(result).to eq "some text"
   end
 end
