2e479f55f905adc77ac8ce52910a973853355e69
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-11-15 14:54:26 +0000

    Updates MusicLibrary unit tests

--- 11_parent_child_classes/spec/music_library_spec.rb
+++ 11_parent_child_classes/spec/music_library_spec.rb
@@ -23,6 +23,23 @@ RSpec.describe MusicLibrary do
       music_library.add(track_2)
       expect(music_library.all).to eq [track_1, track_2]
     end
-  end
 
+    it "searches through tracks" do
+      music_library = MusicLibrary.new
+      track_1 = double(:track, matches?: true)
+      track_2 = double(:track, matches?: false)
+      music_library.add(track_1)
+      music_library.add(track_2)
+      expect(music_library.search("title")).to eq [track_1]
+    end
+
+    it "search doesn't find any matches" do
+      music_library = MusicLibrary.new
+      track_1 = double(:track, matches?: false)
+      track_2 = double(:track, matches?: false)
+      music_library.add(track_1)
+      music_library.add(track_2)
+      expect(music_library.search("title")).to eq []
+    end
+  end
 end
