d0fb5214daa54ce83bda818bdd12a040e23e51db
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-11-15 16:30:53 +0000

    Adds TaskFormatter integration tests

--- 13_peer_classes/challenge/spec/tasks_integration_spec.rb
+++ 13_peer_classes/challenge/spec/tasks_integration_spec.rb
@@ -1,5 +1,6 @@
 require "task_list"
 require "task"
+require "task_formatter"
 
 RSpec.describe "tasks integration" do
   it "adds tasks to a list" do
@@ -20,5 +21,24 @@ RSpec.describe "tasks integration" do
     task_1.mark_complete
     task_2.mark_complete
     expect(task_list.all_complete?).to eq true
+  end
+
+  describe "TaskFormatter and Task" do
+    context "task hasn't been completed" do
+      it "formats the task without a cross" do
+        task = Task.new("my_title")
+        task_formatter = TaskFormatter.new(task)
+        expect(task_formatter.format).to eq "- [ ] my_title"
+      end
+    end
+    
+    context "task is complete" do
+      it "formats the task with a cross" do
+        task = Task.new("my_title")
+        task_formatter = TaskFormatter.new(task)
+        task.mark_complete
+        expect(task_formatter.format).to eq "- [x] my_title"
+      end
+    end
   end
 end
