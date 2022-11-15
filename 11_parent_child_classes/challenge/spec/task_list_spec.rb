b6b1ec83b8f6b5cda0fea75ed589165a71da78fc
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-11-15 15:06:33 +0000

    Adds unit tests with task doubles

--- 11_parent_child_classes/challenge/spec/task_list_spec.rb
+++ 11_parent_child_classes/challenge/spec/task_list_spec.rb
@@ -11,5 +11,43 @@ RSpec.describe TaskList do
     expect(task_list.all_complete?).to eq false
   end
 
-  # Unit test `#all` and `#all_complete?` behaviour
+  it "#all returns a list of all tasks" do
+    task_list = TaskList.new
+    task_1 = double(:task)
+    task_2 = double(:task)
+    task_3 = double(:task)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    task_list.add(task_3)
+    expect(task_list.all).to eq [task_1, task_2, task_3]
+  end
+
+  context "#complete" do
+    it "returns false if list is empty" do
+      task_list = TaskList.new
+      expect(task_list.all_complete?).to eq false
+    end
+
+    it "returns false if one task ins't complete" do
+      task_list = TaskList.new
+      task_1 = double(:task, complete?: true)
+      task_2 = double(:task, complete?: false)
+      task_3 = double(:task, complete?: true)
+      task_list.add(task_1)
+      task_list.add(task_2)
+      task_list.add(task_3)
+      expect(task_list.all_complete?).to eq false
+    end
+
+    it "returns true if all tasks are complete" do
+      task_list = TaskList.new
+      task_1 = double(:task, complete?: true)
+      task_2 = double(:task, complete?: true)
+      task_3 = double(:task, complete?: true)
+      task_list.add(task_1)
+      task_list.add(task_2)
+      task_list.add(task_3)
+      expect(task_list.all_complete?).to eq true
+    end
+  end
 end
