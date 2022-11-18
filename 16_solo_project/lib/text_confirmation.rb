28cba2284f118f223610fd46be603e51b403ebca
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2022-11-18 12:18:31 +0000

    Text spec done

--- 16_solo_project/lib/text_confirmation.rb
+++ 16_solo_project/lib/text_confirmation.rb
@@ -1,20 +1,20 @@
 require 'twilio-ruby'
 
 class TextConfirmation
-  def initialize(requester)
+  def initialize
     @my_mobile = ENV['MY_MOBILE']
     my_auth_token = ENV['TWILIO_TOKEN']
-    account_sid = "ACb008b1d4a2bc236d7b06178ccc9a2042" # Your Test Account SID from www.twilio.com/console/settings
-    auth_token = "#{my_auth_token}"   # Your Test Auth Token from www.twilio.com/console/settings
-    @requester = requester
+    account_sid = "ACb008b1d4a2bc236d7b06178ccc9a2042"
+    auth_token = "#{my_auth_token}"
+    @client = Twilio::REST::Client.new account_sid, auth_token
   end
 
   def send_text
     time = Time.now + 3600
-    @requester.messages.create(
+    @client.messages.create(
     from: "+16614855243",
     to: "+#{@my_mobile}",
-    body: "Thank you! Your order was placed and will be delivered before 1230"
+    body: "Thank you! Your order was placed and will be delivered before #{time}"
     )
   end
 end
--- 16_solo_project/spec/text_spec.rb
+++ 16_solo_project/spec/text_spec.rb
@@ -1,4 +1,21 @@
-require 'text_confirmation'
+class TextConfirmation
+  def initialize(requester)
+    @my_mobile = ENV['MY_MOBILE']
+    my_auth_token = ENV['TWILIO_TOKEN']
+    account_sid = "ACb008b1d4a2bc236d7b06178ccc9a2042"
+    auth_token = "#{my_auth_token}"
+    @requester = requester
+  end
+
+  def send_text
+    time = Time.now + 3600
+    @requester.messages.create(
+    from: "+16614855243",
+    to: "+#{@my_mobile}",
+    body: "Thank you! Your order was placed and will be delivered before 1230"
+    )
+  end
+end
 
 RSpec.describe TextConfirmation do
 
@@ -12,9 +29,8 @@ RSpec.describe TextConfirmation do
       body: "Thank you! Your order was placed and will be delivered before 1230"
       ).and_return("Thank you! Your order was placed and will be delivered before 1230")
     text = TextConfirmation.new(fake_requester)
+    
     expect(text.send_text).to eq "Thank you! Your order was placed and will be delivered before 1230"
   end
 
 end
-
-# '{"body": "Thank you! Your order was placed and will be delivered before 1230", "num_segments": "1", "direction": "outbound-api", "from": "+16614855243", "date_updated": "Fri, 18 Nov 2022 10:44:32 +0000", "price": null, "error_message": null, "uri": "/2010-04-01/Accounts/ACb008b1d4a2bc236d7b06178ccc9a2042/Messages/SM7323b83e60ef003dd7035e19c0660d76.json", "account_sid": "ACb008b1d4a2bc236d7b06178ccc9a2042", "num_media": "0", "to": "+447788930837", "date_created": "Fri, 18 Nov 2022 10:44:32 +0000", "status": "queued", "sid": "SM7323b83e60ef003dd7035e19c0660d76", "date_sent": null, "messaging_service_sid": null, "error_code": null, "price_unit": "USD", "api_version": "2010-04-01", "subresource_uris": {"media": "/2010-04-01/Accounts/ACb008b1d4a2bc236d7b06178ccc9a2042/Messages/SM7323b83e60ef003dd7035e19c0660d76/Media.json"}}'
